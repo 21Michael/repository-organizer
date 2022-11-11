1020ea084c85d179d685f02abfe810b124571970
Mikhail Rudev <mixas769@gmail.com>
2022-11-11 13:23:38 +0200

    add docker

---
+++ deploy/docker-run.sh
@@ -0,0 +1,6 @@
+# 1) build and run
+docker compose up -d --build
+
+## 1) Stop Remove containers
+#docker compose down
+
--- server/docker-run.sh
+++ server/docker-run.sh
@@ -3,5 +3,7 @@ docker build . -t ubuntu:repository-organizer-server
 
 # build container
 docker container run -d -it \
+        -p 4000:4000 \
         --mount type=bind,source="$(pwd)",target=/usr/project/server \
         --name repository-organizer-server ubuntu:repository-organizer-server
+
